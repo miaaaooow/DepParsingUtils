cd427648755917a3ae2fcafec35d2da5041fcf7f
miaaaooow <mateva.maria@gmail.com>
2013-05-21 11:41:22 +0300

    labeled evaluation

--- src/org/bultreebank/expriments/Main.java
+++ src/org/bultreebank/expriments/Main.java
@@ -2,9 +2,12 @@ package org.bultreebank.expriments;
 
 public class Main {
 	public static void main(String[] args) {
-		for (int i = 1; i < 5; i++) {
-			System.out.println("0" + i + "/12/12");
+		for (int i = 15; i < 32; i++) {
+			System.out.println( + i + "/05/13");
 			//System.out.println(i + "/11/12");
 		}
+		int i = 5;
+		double d = 88.123213213;
+		System.out.println((double)d / i);
 	}
 }
--- src/utils/GatherMSTEvaluations.java
+++ src/utils/GatherMSTEvaluations.java
@@ -12,41 +12,62 @@ import java.io.OutputStreamWriter;
 public class GatherMSTEvaluations {
 
 	public static void main(String[] args) {
-		String mstEvalFile = "mst-ord01-iter15-retest.txt";
-		String res = "resultEval1.csv";
+		String mstEvalFile = "voting_unlabeled_avg_acc_2,3,5,7_14.txt";
+		String res = "resultEval_ulab_avg_acc.csv";
 		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(
 					new FileInputStream(new File(mstEvalFile))));
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
 					new FileOutputStream(new File(res))));
-
+			
+			double maxUlab = 0.0;
+			double maxLab = 0.0;
+			
 			String line = "";
+			boolean parsers = false;
 			while ((line = br.readLine()) != null) {
+				if (parsers) {
+					bw.write(line);
+					bw.write("\t");
+					parsers = false;
+				}
+				if (line.startsWith("Parsers chosen for voting: ")) {
+					parsers = true;
+				}
 				if (line.startsWith("Tokens: ")) {
-					bw.write(line.replaceAll("Tokens: ", ""));
+					//bw.write(line.replaceAll("Tokens: ", ""));
+					//bw.write("\t");
+				} else if (line.startsWith("Correct: ")) {
+					//bw.write(line.replaceAll("Correct: ", ""));
+					//bw.write("\t");	
+				} else if (line.startsWith("Unlabeled Accuracy: ")) {
+					String ulabString = line.replaceAll("Unlabeled Accuracy: ", "");
+					double ulab = Double.parseDouble(ulabString);
+					bw.write(ulabString);
+					if (maxUlab < ulab) {
+						maxUlab = ulab;
+					}
 					bw.write("\t");
-				}
-				if (line.startsWith("Correct: ")) {
-					bw.write(line.replaceAll("Correct: ", ""));
-					bw.write("\t");	
-				}
-				if (line.startsWith("Unlabeled Accuracy: ")) {
-					bw.write(line.replaceAll("Unlabeled Accuracy: ", ""));
-					bw.write("\t");
-				}
-				if (line.startsWith("Unlabeled Complete Correct: ")) {
+				} else if (line.startsWith("Unlabeled Complete Correct: ")) {
 					bw.write(line.replaceAll("Unlabeled Complete Correct: ", ""));
-					bw.write("\t");
-				}
-				if (line.startsWith("Labeled Accuracy: ")) {
-					bw.write(line.replaceAll("Labeled Accuracy: ", ""));
-					bw.write("\t");
-				}
-				if (line.startsWith("Labeled Complete Correct: ")) {
-					bw.write(line.replaceAll("Labeled Complete Correct: ", ""));
+				//	bw.write("\t");
 					bw.write("\n");
+				} else if (line.startsWith("Labeled Accuracy: ")) {
+					String labString = line.replaceAll("Labeled Accuracy: ", "");
+					double lab = Double.parseDouble(labString);
+				//	bw.write(labString);
+					if (maxLab < lab) {
+						maxLab = lab;
+					}
+				//	bw.write("\t");
+				} else if (line.startsWith("Labeled Complete Correct: ")) {
+					//bw.write(line.replaceAll("Labeled Complete Correct: ", ""));
+					//bw.write("\n");
+					
 				}
 			}
+			System.out.println(maxUlab);
+			System.out.println(maxLab);			
 			bw.close();
 			br.close();
 
